e4b8fdc35a7269e255fd3c9d1c6fb44620479926
Toshihiko SHIMOKAWA <toshi@p.is.kyusan-u.ac.jp>
2020-06-29 10:39:40 +0900

    HTTP client

---
+++ httpclient2.rb
@@ -0,0 +1,15 @@
+#httpclient2.rb
+
+require "socket"
+
+host="www.is.kyusan-u.ac.jp"
+port=80
+path="/~toshi/index.html"
+
+TCPSocket.open host, port do |sock|
+    sock.puts "GET #{path}"
+
+    while line=sock.gets
+        puts line
+    end
+end
